dc4dec565d1021fc62a4827026ef4f7b0a0513e6
lucianobragaweb <lucianobragaweb@gmail.com>
2023-05-26 15:05:16 -0300

    :bug: fix: fix button

--- src/Providers/CrudifyServiceProvider.php
+++ src/Providers/CrudifyServiceProvider.php
@@ -36,7 +36,7 @@ class CrudifyServiceProvider extends ServiceProvider implements DeferrableProvid
             self::ROOT_PATH.'/resources/views/components/'.config('crudify.theme').'/input-text.blade.php' => resource_path('views/components/crudify/input-text.blade.php'),
             self::ROOT_PATH.'/resources/views/components/'.config('crudify.theme').'/input-number.blade.php' => resource_path('views/components/crudify/input-number.blade.php'),
             self::ROOT_PATH.'/resources/views/components/'.config('crudify.theme').'/input-date.blade.php' => resource_path('views/components/crudify/input-date.blade.php'),
-            self::ROOT_PATH.'/resources/views/components/'.config('crudify.theme').'/form/button.blade.php' => resource_path('views/components/crudify/button.blade.php'),
+            self::ROOT_PATH.'/resources/views/components/'.config('crudify.theme').'/form/button.blade.php' => resource_path('views/components/crudify/form/button.blade.php'),
 
             // View stubs
             self::ROOT_PATH.'/stubs/resources/views/'.config('crudify.theme').'/create.blade.php.stub' => base_path('stubs/crudify/views/create.blade.php.stub'),
